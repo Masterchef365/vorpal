5df05e669fef0a152859f73535a529121d0dbd15
Masterchef365 <duncan.freeman1@gmail.com>
2023-09-28 22:56:57 -0700

    Initial commit, copy egui_node_graph_example

---
+++ vorpal-ui/src/main.rs
@@ -0,0 +1,26 @@
+#![forbid(unsafe_code)]
+#![cfg_attr(not(debug_assertions), deny(warnings))] // Forbid warnings in release builds
+#![warn(clippy::all, rust_2018_idioms)]
+
+use vorpal_ui::NodeGraphExample;
+
+// When compiling natively:
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {
+    use eframe::egui::Visuals;
+
+    eframe::run_native(
+        "Egui node graph example",
+        eframe::NativeOptions::default(),
+        Box::new(|cc| {
+            cc.egui_ctx.set_visuals(Visuals::dark());
+            #[cfg(feature = "persistence")]
+            {
+                Box::new(NodeGraphExample::new(cc))
+            }
+            #[cfg(not(feature = "persistence"))]
+            Box::<NodeGraphExample>::default()
+        }),
+    )
+    .expect("Failed to run native example");
+}
