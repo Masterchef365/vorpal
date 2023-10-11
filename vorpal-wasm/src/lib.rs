fae11529efbb4951a597c5011685a83505b050b0
Masterchef365 <duncan.freeman1@gmail.com>
2023-10-10 23:25:35 -0700

    PREPPING

--- vorpal-wasm/src/lib.rs
+++ vorpal-wasm/src/lib.rs
@@ -1,3 +1,6 @@
+use vorpal_core::*;
+use anyhow::Result;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -11,4 +14,25 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+}
+
+pub fn evaluate_node(node: &Node, ctx: &ExternContext) -> Result<Value> {
+    Engine::new()?.eval(&node)
+}
+
+pub struct Engine {
+    wasm_engine: wasm_bridge::Engine,
+}
+
+impl Engine {
+    pub fn new() -> Result<Self> {
+        todo!()
+    }
+
+    pub fn eval(&mut self, node: &Node) -> Result<Value> {
+        todo!()
+    }
+}
+
+fn compile_to_wat() -> String {
 }
