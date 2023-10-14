75147ad082e567c9bd48a7760ac6e5a6cd87abf3
Masterchef365 <duncan.freeman1@gmail.com>
2023-10-13 20:41:51 -0700

    More "tests"

--- vorpal-wasm/examples/basic.rs
+++ vorpal-wasm/examples/basic.rs
@@ -1,6 +1,6 @@
 use std::rc::Rc;
 
-use vorpal_core::{ComponentInfixOp, DataType, ExternContext, ExternInputId, Value};
+use vorpal_core::{ComponentFn, ComponentInfixOp, DataType, ExternContext, ExternInputId, Value};
 use vorpal_wasm::evaluate_node;
 
 fn main() {
@@ -10,39 +10,46 @@ fn main() {
     ctx.insert_input(&test_input_name, Value::Vec2([420.0, 69.0]));
 
     /*
-       let node = Rc::new(vorpal_core::Node::Make(
-       vec![
-       Rc::new(vorpal_core::Node::Constant(Value::Scalar(1.0))),
-       Rc::new(vorpal_core::Node::Constant(Value::Scalar(1.0))),
-       Rc::new(vorpal_core::Node::Constant(Value::Scalar(1.0))),
-       Rc::new(vorpal_core::Node::Constant(Value::Scalar(1.0))),
-       ],
-       DataType::Vec4,
-       ));
-       */
+    let node = Rc::new(vorpal_core::Node::Make(
+    vec![
+    Rc::new(vorpal_core::Node::Constant(Value::Scalar(1.0))),
+    Rc::new(vorpal_core::Node::Constant(Value::Scalar(1.0))),
+    Rc::new(vorpal_core::Node::Constant(Value::Scalar(1.0))),
+    Rc::new(vorpal_core::Node::Constant(Value::Scalar(1.0))),
+    ],
+    DataType::Vec4,
+    ));
+    */
 
     /*
-       let node = vorpal_core::Node::Constant(Value::Scalar(3.0));
+    let node = vorpal_core::Node::Constant(Value::Scalar(3.0));
 
-       let node = Rc::new(vorpal_core::Node::ComponentFn(
-       vorpal_core::ComponentFn::Abs,
-       Rc::new(node),
-       ));
-       */
+    let node = Rc::new(vorpal_core::Node::ComponentFn(
+    vorpal_core::ComponentFn::Abs,
+    Rc::new(node),
+    ));
+    */
 
-    let a = vorpal_core::Node::Constant(Value::Scalar(3.0));
+    let a = vorpal_core::Node::Constant(Value::Scalar(2.0));
     let a = Rc::new(a);
 
-    let b = vorpal_core::Node::Constant(Value::Scalar(2.0));
+    let b = vorpal_core::Node::Constant(Value::Scalar(-3.5));
     let b = Rc::new(b);
 
     for op in ComponentInfixOp::all() {
-        println!("3 {} 2", op.symbol());
+        println!("2 {} -3.5", op.symbol());
         let node = Rc::new(vorpal_core::Node::ComponentInfixOp(
-                a.clone(),
-                op,
-                b.clone()
+            a.clone(),
+            op,
+            b.clone(),
         ));
+
+        println!("{:?}", evaluate_node(&node, &ctx).unwrap());
+    }
+
+    for func in ComponentFn::all() {
+        println!("{}(-3.5)", func.symbol());
+        let node = Rc::new(vorpal_core::Node::ComponentFn(func, a.clone()));
 
         println!("{:?}", evaluate_node(&node, &ctx).unwrap());
     }
