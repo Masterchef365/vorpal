aee6b38d164adf4e4b994d3b55d5e31691f5da7f
Masterchef365 <duncan.freeman1@gmail.com>
2023-10-17 01:18:01 -0700

    Use a larger array

--- vorpal-ui/src/app.rs
+++ vorpal-ui/src/app.rs
@@ -47,7 +47,7 @@ impl Default for NodeGraphExample {
             time: Instant::now(),
             nodes,
             image: Default::default(),
-            image_data: NdArray::zeros(vec![100, 100, 4]),
+            image_data: NdArray::zeros(vec![500, 500, 4]),
         }
     }
 }
