28a6d9bbf02ffdc5ab12c27b97cbba69ac74d5db
Duncan Freeman <duncan.freeman1@gmail.com>
2024-02-29 02:17:28 -0800

    Implement splat and normalize

--- vorpal-core/src/highlevel.rs
+++ vorpal-core/src/highlevel.rs
@@ -1,6 +1,6 @@
-use std::{rc::Rc, collections::HashMap};
+use std::{collections::HashMap, rc::Rc};
 
-use crate::{ComponentFn, ComponentInfixOp, DataType, ExternInputId, Node, Value, HashRcByPtr};
+use crate::{ComponentFn, ComponentInfixOp, DataType, ExternInputId, HashRcByPtr, Node, Value};
 
 /// A higher-level, nicer set of nodes. Compiles to the lower-level set ...
 /// HighNode is a strict superset of Node. Is always directly convertible to Node.
@@ -16,7 +16,8 @@ pub enum HighNode {
     Dot(Rc<HighNode>, Rc<HighNode>),
 
     // New stuff!
-    Normalize(Rc<HighNode>),
+    Normalize(Rc<HighNode>, DataType),
+    Splat(Rc<HighNode>, DataType),
     Swizzle(Swizzle, Rc<HighNode>),
 }
 
@@ -44,31 +45,57 @@ fn convert_rc_highnode(high: Rc<HighNode>, cache: &mut Cache) -> Rc<Node> {
 }
 
 fn lower_node_recursive(high: Rc<HighNode>, cache: &mut Cache) -> Rc<Node> {
-    Rc::new(match Rc::unwrap_or_clone(high) {
-        HighNode::ExternInput(id, dtype) => Node::ExternInput(id, dtype),
-        HighNode::Constant(value) => Node::Constant(value),
-        HighNode::Make(components, dtype) => Node::Make(
-            components.into_iter().map(|c| convert_rc_highnode(c, cache)).collect(),
+    match Rc::unwrap_or_clone(high) {
+        HighNode::ExternInput(id, dtype) => Rc::new(Node::ExternInput(id, dtype)),
+        HighNode::Constant(value) => Rc::new(Node::Constant(value)),
+        HighNode::Make(components, dtype) => Rc::new(Node::Make(
+            components
+                .into_iter()
+                .map(|c| convert_rc_highnode(c, cache))
+                .collect(),
             dtype,
-        ),
-        HighNode::ComponentInfixOp(left, op, right) => {
-            Node::ComponentInfixOp(convert_rc_highnode(left, cache), op, convert_rc_highnode(right, cache))
+        )),
+        HighNode::ComponentInfixOp(left, op, right) => Rc::new(Node::ComponentInfixOp(
+            convert_rc_highnode(left, cache),
+            op,
+            convert_rc_highnode(right, cache),
+        )),
+        HighNode::ComponentFn(op, data) => {
+            Rc::new(Node::ComponentFn(op, convert_rc_highnode(data, cache)))
         }
-        HighNode::ComponentFn(op, data) => Node::ComponentFn(op, convert_rc_highnode(data, cache)),
-        HighNode::GetComponent(left, right) => {
-            Node::GetComponent(convert_rc_highnode(left, cache), convert_rc_highnode(right, cache))
-        }
-        HighNode::Dot(left, right) => {
-            Node::Dot(convert_rc_highnode(left, cache), convert_rc_highnode(right, cache))
-        }
-        /*
+        HighNode::GetComponent(left, right) => Rc::new(Node::GetComponent(
+            convert_rc_highnode(left, cache),
+            convert_rc_highnode(right, cache),
+        )),
+        HighNode::Dot(left, right) => Rc::new(Node::Dot(
+            convert_rc_highnode(left, cache),
+            convert_rc_highnode(right, cache),
+        )),
         // Now here's the more useful stuff
-        HighNode::Normalize(vect) => {
-            let vect = convert_rc_highnode(vect);
-            let v2 = Rc::new(Node::Dot(vect.clone(), vect.clone()));
-
+        HighNode::Splat(scalar, dtype) => {
+            let scalar = convert_rc_highnode(scalar, cache);
+            let copies = (0..dtype.n_lanes()).map(|_| scalar.clone()).collect();
+            Rc::new(Node::Make(copies, dtype))
         }
-        */
+        HighNode::Normalize(vect, dtype) => {
+            let len2 = Rc::new(HighNode::Dot(vect.clone(), vect.clone()));
+
+            let half = Rc::new(HighNode::Constant(Value::Scalar(0.5)));
+            let len = Rc::new(HighNode::ComponentInfixOp(
+                len2,
+                ComponentInfixOp::Power,
+                half,
+            ));
+            let len = Rc::new(HighNode::Splat(len, dtype));
+
+            let normed = Rc::new(HighNode::ComponentInfixOp(
+                vect.clone(),
+                ComponentInfixOp::Divide,
+                len,
+            ));
+
+            convert_rc_highnode(normed, cache)
+        }
         _ => todo!(),
-    })
+    }
 }
